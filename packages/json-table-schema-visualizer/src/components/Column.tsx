bf0f0e040d08ab1e5d222e8a9964ef743330d7df
bocovo <bocovojuste@gmail.com>
2024-03-28 21:27:31 +0100

    feat(visualizer): create column component
    Create the component that represents database column in the diagram

---
+++ packages/json-table-schema-visualizer/src/components/Column.tsx
@@ -0,0 +1,63 @@
+import { COLUMN_HEIGHT, PADDINGS, TABLE_WIDTH } from "@/constants/sizing";
+import { Group, Rect } from "react-konva";
+import KonvaText from "./dumb/KonvaText";
+import { useTheme } from "@/hooks/theme";
+import { useState } from "react";
+
+interface ColumnProps {
+  colName: string;
+  type: string;
+  isPrimaryKey: boolean;
+}
+
+const Column = ({ colName, type, isPrimaryKey = false }: ColumnProps) => {
+  const theme = useTheme();
+  const [hovered, setHovered] = useState(false);
+
+  const handleOnHover = () => {
+    setHovered(true);
+  };
+
+  const handleOnLeave = () => {
+    setHovered(false);
+  };
+
+  const colTextColor = theme.text[900];
+  const typeTextColor = theme.text[700];
+  const fontStyle = isPrimaryKey ? "bold" : "normal";
+
+  return (
+    <Group>
+      <Rect
+        fill={hovered ? "aliceblue" : "transparent"}
+        width={TABLE_WIDTH}
+        height={COLUMN_HEIGHT}
+      />
+
+      <KonvaText
+        ellipsis
+        wrap="none"
+        text={colName}
+        fill={colTextColor}
+        width={TABLE_WIDTH}
+        fontStyle={fontStyle}
+        padding={PADDINGS.sm}
+        height={COLUMN_HEIGHT}
+      />
+
+      <KonvaText
+        text={type}
+        align="right"
+        width={TABLE_WIDTH}
+        fill={typeTextColor}
+        padding={PADDINGS.sm}
+        fontStyle={fontStyle}
+        height={COLUMN_HEIGHT}
+        onMouseOver={handleOnHover}
+        onMouseLeave={handleOnLeave}
+      />
+    </Group>
+  );
+};
+
+export default Column;
