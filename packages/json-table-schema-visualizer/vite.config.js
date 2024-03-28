d1ae0f38f8e6fe0febccfde1479a8d4bd35e19ce
bocovo <bocovojuste@gmail.com>
2024-03-28 19:35:14 +0100

    chore(visualizer): add vite

---
+++ packages/json-table-schema-visualizer/vite.config.js
@@ -0,0 +1,11 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
