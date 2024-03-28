7ce80ba314f18afe5abc613c233242eff37f82c3
bocovo <bocovojuste@gmail.com>
2024-03-28 21:25:18 +0100

    feat(visualizer): allow wrapping konva shape with decorator
    Use of storybook story parameter to indicate if the story must be wrapped
    with konva layers and theme provider

--- packages/json-table-schema-visualizer/.storybook/preview.jsx
+++ packages/json-table-schema-visualizer/.storybook/preview.jsx
@@ -1,3 +1,8 @@
+import React from "react";
+import { Layer, Stage } from "react-konva";
+import { defaultThemeConfig } from "../src/constants/theme";
+import ThemeProvider from "../src/providers/ThemeProvider";
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -8,6 +13,20 @@ const preview = {
       },
     },
   },
+  decorators: [
+    (Story, configs) =>
+      configs.parameters.withKonvaWrapper ? (
+        <ThemeProvider theme={defaultThemeConfig}>
+          <Stage width={window.innerWidth} height={window.innerHeight}>
+            <Layer>
+              <Story />
+            </Layer>
+          </Stage>
+        </ThemeProvider>
+      ) : (
+        <Story />
+      ),
+  ],
 };
 
 export default preview;
