c097fcc4e516fbad37f0379a9fdb933dd638fcc2
bocovo <bocovojuste@gmail.com>
2024-03-28 21:25:48 +0100

    chore(visualizer): create some constants

---
+++ packages/json-table-schema-visualizer/src/constants/sizing.ts
@@ -0,0 +1,16 @@
+export const TABLE_WIDTH = 180;
+export const TABLE_COLOR_HEIGHT = 5;
+export const TABLE_LINE_HEIGHT = 25;
+export const COLUMN_HEIGHT = 30;
+export const CONNECTION_STROKE = 2;
+export const DEFAULT_PADDING = 5;
+export const CROSS_CONNECTION_MIN_MARGIN = 20;
+export const CONNECTION_MARGIN = 40;
+export const PADDINGS = {
+  sm: 8,
+};
+
+export const FONT_SIZES = {
+  md: 13,
+  lg: 15,
+};
