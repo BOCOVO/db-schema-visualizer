9cc02f11e286a0edeb73b62ec47df083654ee511
bocovo <bocovojuste@gmail.com>
2024-04-21 01:58:10 +0100

    chore(dbml2json): add schema validator

--- packages/dbml-to-json-table-schema/src/index.ts
+++ packages/dbml-to-json-table-schema/src/index.ts
@@ -1,12 +1,12 @@
 import { Parser } from "@dbml/core";
 
 import { dbmlSchemaToJSONTableSchema } from "./utils/transfomers/dbmlSchemaToJSONTableSchema";
+import { validateSchema } from "./validators";
 
-import type { JSONTableSchema } from "shared/types/tableSchema"
+import type { JSONTableSchema } from "shared/types/tableSchema";
 
-export const parseDBMLToJSON = (
-  dbmlCode: string,
-): JSONTableSchema => {
+export const parseDBMLToJSON = (dbmlCode: string): JSONTableSchema => {
   const rawParsedSchema = Parser.parseDBMLToJSON(dbmlCode);
+  validateSchema(rawParsedSchema);
   return dbmlSchemaToJSONTableSchema(rawParsedSchema);
 };
