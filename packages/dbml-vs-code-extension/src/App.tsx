5fa5e0a8b29b294a8d5c3488f0dfd9b73f7cfedc
bocovo <bocovojuste@gmail.com>
2024-04-27 22:18:11 +0100

    chore(ext): added theme toggling features

--- packages/dbml-vs-code-extension/src/App.tsx
+++ packages/dbml-vs-code-extension/src/App.tsx
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
 import { JSONTableSchema } from "shared/types/tableSchema";
 import DiagramViewer from "json-table-schema-visualizer/src/components/DiagramViewer/DiagramViewer";
-import { defaultThemeConfig } from "json-table-schema-visualizer/src/constants/theme";
+import { useCreateTheme } from "json-table-schema-visualizer/src/hooks/theme";
+import ThemeProvider from "json-table-schema-visualizer/src/providers/ThemeProvider";
+import { Theme } from "json-table-schema-visualizer/src/types/theme";
+import {
+  WebviewCommand,
+  WebviewPostMessage,
+} from "@/extension/types/webviewCommand";
 
 const App = () => {
   const [schema, setSchema] = useState<JSONTableSchema | null>(null);
+  const { setTheme, theme, themeColors } = useCreateTheme(
+    window.EXTENSION_DEFAULT_CONFIG?.theme,
+  );
 
   useEffect(() => {
     window.addEventListener("message", (e: MessageEvent) => {
@@ -19,7 +28,32 @@ const App = () => {
     return "Loading...";
   }
 
-  return <DiagramViewer {...schema} theme={defaultThemeConfig} />;
+  // update the preference in the extension settings
+  const saveThemePreference = (theme: Theme) => {
+    setTheme(theme);
+    const updateThemeMessage: WebviewPostMessage = {
+      command: WebviewCommand.SET_THEME_PREFERENCES,
+      message: theme,
+    };
+
+    if (!window.vsCodeWebviewAPI) {
+      console.error(
+        "can't send message to extension due vsCodeWebviewAPI global variable is not defined",
+      );
+    } else {
+      window.vsCodeWebviewAPI?.postMessage(updateThemeMessage);
+    }
+  };
+
+  return (
+    <ThemeProvider
+      theme={theme}
+      setTheme={saveThemePreference}
+      themeColors={themeColors}
+    >
+      <DiagramViewer {...schema} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
