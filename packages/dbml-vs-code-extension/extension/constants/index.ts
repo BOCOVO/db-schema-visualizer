30c8b9c19b169a945491e2c2ed5317e56f3157f3
bocovo <bocovojuste@gmail.com>
2024-04-27 22:11:26 +0100

    chore(ext): added extension configs

--- packages/dbml-vs-code-extension/extension/constants/index.ts
+++ packages/dbml-vs-code-extension/extension/constants/index.ts
@@ -1,3 +1,5 @@
 export const WEB_VIEW_NAME = "dblm-preview-webview";
 export const WEB_VIEW_TITLE = "DBLM Diagram Preview";
 export const DIAGRAM_UPDATER_DEBOUNCE_TIME = 500;
+export const EXTENSION_CONFIG_SESSION = "dbmlERDPreviewer";
+export const WEBVIEW_HTML_MARKER_FOR_DEFAULT_CONFIG = "// <%DEFAULT-SCRIPT%>";
--- packages/dbml-vs-code-extension/extension/views/helper.ts
+++ packages/dbml-vs-code-extension/extension/views/helper.ts
@@ -1,10 +1,29 @@
 import { ExtensionContext, Webview } from "vscode";
 
 export class WebviewHelper {
-  public static setupHtml(webview: Webview, context: ExtensionContext) {
-    return process.env.VITE_DEV_SERVER_URL
+  public static setupHtml(
+    webview: Webview,
+    context: ExtensionContext,
+    defaultConfig: DefaultPageConfig,
+  ): string {
+    const html = process.env.VITE_DEV_SERVER_URL
       ? /* @ts-ignore */
         /* @ts-ignore */ __getWebviewHtml__(process.env.VITE_DEV_SERVER_URL)
       : __getWebviewHtml__(webview, context);
+
+    return WebviewHelper.injectDefaultConfig(html, defaultConfig);
+  }
+
+  public static injectDefaultConfig(
+    html: string,
+    configs: DefaultPageConfig,
+  ): string {
+    return html.replace(
+      WEBVIEW_HTML_MARKER_FOR_DEFAULT_CONFIG,
+      `
+       window.EXTENSION_DEFAULT_CONFIG = ${JSON.stringify(configs)};
+      `,
+    );
+  }
   }
 }
