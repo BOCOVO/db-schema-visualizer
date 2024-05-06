55853966e619eed0d08695dda7cb4b67b26b4a9f
bocovo <bocovojuste@gmail.com>
2024-05-06 23:57:47 +0100

    chore(visualizer): compute table W while computing table dimension
    Before this commit table width is static.

--- packages/json-table-schema-visualizer/src/utils/computeTableDimension.ts
+++ packages/json-table-schema-visualizer/src/utils/computeTableDimension.ts
@@ -1,15 +1,14 @@
 import { type JSONTableTable } from "shared/types/tableSchema";
 
-import {
-  COLUMN_HEIGHT,
-  TABLE_HEADER_HEIGHT,
-  TABLE_WIDTH,
-} from "@/constants/sizing";
+import { getTableLinesText } from "./tableWComputation/getTableLinesText";
+import { computeTablePreferredWidth } from "./tableWComputation/computeTablePreferredWidth";
+
+import { COLUMN_HEIGHT, TABLE_HEADER_HEIGHT } from "@/constants/sizing";
 import { type Dimension } from "@/types/dimension";
 
 export const computeTableDimension = (table: JSONTableTable): Dimension => {
-  // use the default width for tables
-  const width = TABLE_WIDTH;
+  const tableTexts = getTableLinesText(table.fields, table.name);
+  const width = computeTablePreferredWidth(tableTexts);
   const height = TABLE_HEADER_HEIGHT + COLUMN_HEIGHT * table.fields.length;
 
   return { width, height };
