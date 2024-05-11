ae9ae3d3e1fbdfda96a172e08709cb70f1ebfbde
bocovo <bocovojuste@gmail.com>
2024-05-11 16:43:45 +0100

    feat(dbml-ext): save table position on exiting

--- packages/dbml-vs-code-extension/src/index.tsx
+++ packages/dbml-vs-code-extension/src/index.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from "react-dom/client";
-import '@tomjs/vscode-extension-webview/client';
+import "@tomjs/vscode-extension-webview/client";
+import { tableCoordsStore } from "json-table-schema-visualizer/src/stores/tableCoords";
 import App from "./App";
+
+// save current table position when exiting the page
+window.addEventListener("unload", () => {
+  tableCoordsStore.saveCurrentStore();
+});
 
 const View = () => {
   return <App />;
