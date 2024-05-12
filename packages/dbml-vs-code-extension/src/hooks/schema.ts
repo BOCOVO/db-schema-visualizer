3075792ee1d43e8604924f16ab417df9332ddd29
bocovo <bocovojuste@gmail.com>
2024-05-12 17:33:41 +0100

    chore(dbml-ext): provide tables for position computing while switching schema

--- packages/dbml-vs-code-extension/src/hooks/schema.ts
+++ packages/dbml-vs-code-extension/src/hooks/schema.ts
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import { JSONTableSchema } from "shared/types/tableSchema";
 import { tableCoordsStore } from "json-table-schema-visualizer/src/stores/tableCoords";
 import { stageStateStore } from "json-table-schema-visualizer/src/stores/stagesState";
+import { SetSchemaCommandPayload } from "@/extension/types/webviewCommand";
 
 export const useSchema = () => {
   const [schema, setSchema] = useState<JSONTableSchema | null>(null);
   const [schemaKey, setSchemaKey] = useState<string | null>(null);
 
   const updater = (e: MessageEvent) => {
-    const message = e.data;
+    const message = e.data as SetSchemaCommandPayload;
     if (
       !(message.type === "setSchema" && typeof message.payload === "object")
     ) {
@@ -17,7 +18,7 @@ export const useSchema = () => {
 
     if (message.key && message.key !== schemaKey) {
       // update stores
-      tableCoordsStore.switchTo(message.key);
+      tableCoordsStore.switchTo(message.key, message.payload.tables);
       stageStateStore.switchTo(message.key);
 
       setSchemaKey(message.key);
