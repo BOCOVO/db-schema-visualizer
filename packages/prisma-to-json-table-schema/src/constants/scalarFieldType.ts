70fcf267d5a888a1c6a42b809d0d2adc90556a60
bocovo <bocovojuste@gmail.com>
2024-05-20 03:14:47 +0100

    chore(prisma-parser): create constant for prisma scalar col type

---
+++ packages/prisma-to-json-table-schema/src/constants/scalarFieldType.ts
@@ -0,0 +1,12 @@
+export const scalarFieldType = new Set<string>([
+  "String",
+  "Boolean",
+  "Int",
+  "BigInt",
+  "Float",
+  "Decimal",
+  "DateTime",
+  "Json",
+  "Bytes",
+  "Unsupported",
+]);
