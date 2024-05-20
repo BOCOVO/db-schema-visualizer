480bc560dfd05f2b6db946a463197388182e552e
bocovo <bocovojuste@gmail.com>
2024-05-20 03:31:51 +0100

    chore(prisma-parser): create utility to create intermediate field

---
+++ packages/prisma-to-json-table-schema/src/utils/transformers/intermediate/formatIntermediateField.ts
@@ -0,0 +1,20 @@
+import { getFieldTypeName } from "../getFieldTypeName";
+import { getFieldConfig } from "../getFieldConfig";
+
+import type { IntermediateField } from "@/types/intermediateFormattedNode";
+import type { Field } from "@mrleebo/prisma-ast";
+
+export const formatIntermediateTableField = (
+  node: Field,
+): IntermediateField => {
+  const fieldConfig = getFieldConfig(node.attributes);
+
+  return {
+    name: node.name,
+    type: {
+      type_name: getFieldTypeName(node.fieldType),
+      many: node.array,
+    },
+    ...fieldConfig,
+  };
+};
