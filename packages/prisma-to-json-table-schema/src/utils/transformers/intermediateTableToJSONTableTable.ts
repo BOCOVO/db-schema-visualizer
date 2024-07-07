2d87a0c937cd6812c2bed094cb981e421a5f8508
bocovo <bocovojuste@gmail.com>
2024-07-07 14:05:45 +0100

    feat(prisma-parser): exclude virtual fields/columns

--- packages/prisma-to-json-table-schema/src/utils/transformers/intermediateTableToJSONTableTable.ts
+++ packages/prisma-to-json-table-schema/src/utils/transformers/intermediateTableToJSONTableTable.ts
@@ -11,15 +11,33 @@ export const intermediateTableToJSONTableTable = (
   { fields: intermediateFields, name, indexes }: IntermediateTable,
   enumsSet: Set<string>,
   fieldRelationTable: FieldRelationsMap,
+  tablesSet: Set<string>,
 ): JSONTableTable => {
-  const fields = intermediateFields.map((intermediateField) =>
-    intermediateFieldToJSONTableField(
+  const fields: JSONTableTable["fields"] = [];
+
+  for (let index = 0; index < intermediateFields.length; index++) {
+    const intermediateField = intermediateFields[index];
+
+    /* 
+    VirtualReferenceField is field that not a column in the database
+    but just a reference field for Prisma to allow navigation between
+    relationships.
+    */
+    const isVirtualReferenceField = tablesSet.has(
+      intermediateField.type.type_name,
+    );
+    if (isVirtualReferenceField) {
+      continue;
+    }
+
+    const field = intermediateFieldToJSONTableField(
       name,
       intermediateField,
       enumsSet,
       fieldRelationTable,
-    ),
-  );
+    );
+    fields.push(field);
+  }
 
   return { name, fields, indexes };
 };
