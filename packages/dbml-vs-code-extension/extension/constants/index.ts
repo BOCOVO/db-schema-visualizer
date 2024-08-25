c8e839f7c745e9973309d7c16f0596941697594c
bocovo <bocovojuste@gmail.com>
2024-08-25 14:18:02 +0100

    chore(dbml-ext): fix typo

--- packages/dbml-vs-code-extension/extension/constants/index.ts
+++ packages/dbml-vs-code-extension/extension/constants/index.ts
@@ -1,3 +1,13 @@
+// In the text below, you may notice a spelling
+// mistake: dblm instead of dbml.
+// But by changing this, users will lose their table
+// position if the plugin version is updated.
+// This constant is the identifier given to the webview
+// displaying the diagram, and since the positions of
+// tables and so on are stored in the localStorage, a
+// change to this ID will cause this data to be lost,
+// as if a brand new webview were created.
 export const WEB_VIEW_NAME = "dblm-preview-webview";
-export const WEB_VIEW_TITLE = "DBLM Diagram Preview";
+
+export const WEB_VIEW_TITLE = "DBML Diagram Preview";
 export const EXTENSION_CONFIG_SESSION = "dbmlERDPreviewer";
