9d015926ffac6df4c990c986bb05bbf6e6b7e141
bocovo <bocovojuste@gmail.com>
2024-12-10 01:25:40 +0100

    feat(dbml-parser): transform parsing error location

--- packages/dbml-to-json-table-schema/src/index.ts
+++ packages/dbml-to-json-table-schema/src/index.ts
@@ -1,4 +1,5 @@
-import { Parser } from "@dbml/core";
+import { Parser, type CompilerDiagnostic } from "@dbml/core";
+import { DiagnosticError } from "shared/types/diagnostic";
 
 import { dbmlSchemaToJSONTableSchema } from "./utils/transfomers/dbmlSchemaToJSONTableSchema";
 import { validateSchema } from "./validators";
@@ -6,7 +7,35 @@ import { validateSchema } from "./validators";
 import type { JSONTableSchema } from "shared/types/tableSchema";
 
 export const parseDBMLToJSON = (dbmlCode: string): JSONTableSchema => {
-  const rawParsedSchema = Parser.parseDBMLToJSON(dbmlCode);
-  validateSchema(rawParsedSchema);
-  return dbmlSchemaToJSONTableSchema(rawParsedSchema);
+  try {
+    const rawParsedSchema = Parser.parseDBMLToJSON(dbmlCode);
+    validateSchema(rawParsedSchema);
+    return dbmlSchemaToJSONTableSchema(rawParsedSchema);
+  } catch (error) {
+    if ("location" in (error as any) && "message" in (error as any)) {
+      const _error = error as CompilerDiagnostic;
+
+      const locationEnd = _error.location.end;
+      const locationStart = _error.location.start;
+
+      if (locationEnd === undefined || locationStart === undefined) {
+        throw error;
+      }
+
+      throw new DiagnosticError(
+        {
+          end: {
+            column: locationEnd.column - 1,
+            line: locationEnd.line - 1,
+          },
+          start: {
+            column: locationStart.column - 1,
+            line: locationStart.line - 1,
+          },
+        },
+        _error.message,
+      );
+    }
+    throw error;
+  }
 };
