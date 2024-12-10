86e80a6016afd77b872d95eebf5c706bf1a4997c
bocovo <bocovojuste@gmail.com>
2024-12-10 01:25:57 +0100

    feat(prisma-parser): transform parsing error location

--- packages/prisma-to-json-table-schema/src/index.ts
+++ packages/prisma-to-json-table-schema/src/index.ts
@@ -1,9 +1,52 @@
-import { getSchema } from "@mrleebo/prisma-ast";
+import { type CstNodeLocation, getSchema } from "@mrleebo/prisma-ast";
 import { type JSONTableSchema } from "shared/types/tableSchema";
+import { DiagnosticError } from "shared/types/diagnostic";
 
 import { prismaASTToJSONTableSchema } from "./utils/transformers/prismaASTToJSONTableSchema";
 
 export const parsePrismaToJSON = (prismaCode: string): JSONTableSchema => {
-  const rawParsedSchema = getSchema(prismaCode);
-  return prismaASTToJSONTableSchema(rawParsedSchema);
+  try {
+    const rawParsedSchema = getSchema(prismaCode);
+    return prismaASTToJSONTableSchema(rawParsedSchema);
+  } catch (error) {
+    if ("token" in (error as any)) {
+      const token = (error as any).token as CstNodeLocation;
+
+      const endColumn = token.endColumn;
+      const endLine = token.endLine;
+      const startColumn = token.startColumn;
+      const startLine = token.startLine;
+
+      if (
+        endColumn === undefined ||
+        startColumn === undefined ||
+        endLine === undefined ||
+        startLine === undefined
+      ) {
+        throw error;
+      }
+
+      const locationEnd = { column: endColumn, line: endLine };
+      const locationStart = {
+        column: startColumn,
+        line: startLine,
+      };
+
+      throw new DiagnosticError(
+        {
+          end: {
+            column: locationEnd.column - 1,
+            line: locationEnd.line - 1,
+          },
+          start: {
+            column: locationStart.column - 1,
+            line: locationStart.line - 1,
+          },
+        },
+        String((error as Error).message),
+      );
+    }
+
+    throw error;
+  }
 };
